974aab91c896dbdfab3a37e9dc1a04ae6ef6c1ca
Sirigiri Leela Krishna Reddy <sirigirileelakrishna1133@gmail.com>
2026-04-25 09:44:51 +0530

    Delete components directory

--- components/model.py
+++
@@ -1,5 +0,0 @@
-from langchain.chat_models import ChatOpenAI
-
-def generate_answer(prompt):
-    llm = ChatOpenAI(temperature=0)
-    return llm.predict(prompt)
--- components/retriever.py
+++
@@ -1,2 +0,0 @@
-def retrieve_docs(db, query, k=3):
-    return db.similarity_search(query, k=k)
